c44bd08cbd2d3ff5b8904f1bd09eef7db2bc1e52
leventfatihkaraer <leventfatihkaraer@gmail.com>
2013-06-27 10:24:12 +0300

    Update GitTry.java
    ehe :)

--- GitProject/mypackage/GitTry.java
+++ GitProject/mypackage/GitTry.java
@@ -6,7 +6,7 @@ public class GitTry {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("I've deleted your code :)");
+		System.out.println("I'll kick you off :)");
 	}
 
 }
