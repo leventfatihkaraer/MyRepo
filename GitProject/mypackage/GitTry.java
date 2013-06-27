624f3987b536a5980df68a46412900b58e3a9b13
leventfatihkaraer <leventfatihkaraer@gmail.com>
2013-06-27 10:35:28 +0300

    Update GitTry.java

--- GitProject/mypackage/GitTry.java
+++ GitProject/mypackage/GitTry.java
@@ -6,7 +6,7 @@ public class GitTry {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("I've deleted your code :)");
+		System.out.println("Thank you for contributing osmancan :)");
 	}
 
 }
