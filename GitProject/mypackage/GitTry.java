6b9b7c277a71c247697b1d9051c412dd3787581b
Levent Fatih KARAER <leventfatihkaraer@gmail.com>
2013-06-27 17:07:41 +0300

    yeni commit yaptım. Pek de bisey degistirmedim.

--- GitProject/mypackage/GitTry.java
+++ GitProject/mypackage/GitTry.java
@@ -1,10 +1,7 @@
 package mypackage;
 
 public class GitTry {
-
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 		System.out.println("Thank you for contributing osmancan :)");
 	}
