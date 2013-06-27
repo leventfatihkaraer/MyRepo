edbd5e925e89ddfde1504999cb252043fc599025
leventfatihkaraer <leventfatihkaraer@gmail.com>
2013-06-27 17:12:11 +0300

    Update GitTry.java
    Adımla yasımı ekledim..

--- GitProject/mypackage/GitTry.java
+++ GitProject/mypackage/GitTry.java
@@ -4,6 +4,9 @@ public class GitTry {
 	
 	public static void main(String[] args) {
 		System.out.println("Thank you for contributing osmancan :)");
+		System.out.println("Birazda variable tanımlayalım bakalım");
+		int yas = 24;
+		String adim = "Levent";
 	}
 
 }
